59d329add68161300b46a2403fe1d3b7c0f4a7a1
byeongjuPark <bottlepark28@gmail.com>
2025-07-19 07:15:41 +0900

    create project BE/FE

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button @click="fetchText">API 호출</button>
+    <div v-if="result">{{ result }}</div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      result: ''
+    };
+  },
+  methods: {
+    async fetchText() {
+      try {
+        // 컨테이너 환경에서는 반드시 'http://backend:3000/'으로 호출
+        const response = await axios.get('http://backend:3000/');
+        this.result = response.data;
+      } catch (error) {
+        this.result = 'API 호출 에러: ' + error.message;
+      }
+    }
+  }
+};
+</script>
