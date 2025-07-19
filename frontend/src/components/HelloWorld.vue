e8a1df344292a7e51d3536d77df59846112554cd
byeongjuPark <bottlepark28@gmail.com>
2025-07-19 20:54:53 +0900

    rebuilding frontend

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,29 +1,44 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
 <template>
-  <div>
-    <button @click="fetchText">API 호출</button>
-    <div v-if="result">{{ result }}</div>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
   </div>
 </template>
 
-<script>
-import axios from 'axios';
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
 
-export default {
-  data() {
-    return {
-      result: ''
-    };
-  },
-  methods: {
-    async fetchText() {
-      try {
-        // 컨테이너 환경에서는 반드시 'http://backend:3000/'으로 호출
-        const response = await axios.get('http://backend:3000/');
-        this.result = response.data;
-      } catch (error) {
-        this.result = 'API 호출 에러: ' + error.message;
-      }
-    }
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
   }
-};
-</script>
+}
+</style>
